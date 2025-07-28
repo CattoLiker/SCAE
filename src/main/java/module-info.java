0784e836a5a8116b5806ddc8829e188ce66c3bd8
USER <USER@LAPTOP-HT6D6UPR>
2025-07-28 10:43:25 -0300

    First commit!

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.scaeproyecto.sistemacontrolalmuerzoescolarctn {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    
     opens com.scaeproyecto.sistemacontrolalmuerzoescolarctn to javafx.fxml;
     exports com.scaeproyecto.sistemacontrolalmuerzoescolarctn;
 }
