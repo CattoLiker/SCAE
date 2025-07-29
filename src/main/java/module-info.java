947f96281f6eedd82217bb58d4daab0401518ff7
USER <USER@LAPTOP-HT6D6UPR>
2025-07-29 11:08:52 -0300

    First commit!

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.scaeproyecto.sistemacontrolalmuerzoescolarctn {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.base;
     
     opens com.scaeproyecto.sistemacontrolalmuerzoescolarctn to javafx.fxml;
     exports com.scaeproyecto.sistemacontrolalmuerzoescolarctn;
