5f77631234a4a5e18fd0d25464bcf34c8eb72652
USER <USER@LAPTOP-HT6D6UPR>
2025-07-29 11:44:10 -0300

    First commit!

---
+++ src/main/java/com/scaeproyecto/sistemacontrolalmuerzoescolarctn/AlumnoController.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.scaeproyecto.sistemacontrolalmuerzoescolarctn;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+/**
+ * FXML Controller class
+ *
+ * @author USER
+ */
+public class AlumnoController implements Initializable {
+
+
+    @FXML
+    private TextField TxtCodigo;
+    @FXML
+    private TextField TxtNombre;
+    @FXML
+    private TextField TxtApellido;
+    @FXML
+    private TextField TxtBuscar;
+    @FXML
+    private TableView<?> TablaClientes;
+    @FXML
+    private TableColumn<?, ?> ColumnId;
+    @FXML
+    private TableColumn<?, ?> ColumnNombre;
+    @FXML
+    private TableColumn<?, ?> ColumnApellido;
+    @FXML
+    private Button BtnNuevo;
+    @FXML
+    private Button BtnModificar;
+    @FXML
+    private Button BtnEliminar;
+    @FXML
+    private Button BtnGuardar;
+    @FXML
+    private Button BtnCancelar;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void buscar(KeyEvent event) {
+    }
+
+    @FXML
+    private void mostrarFila(MouseEvent event) {
+    }
+
+    @FXML
+    private void nuevo(ActionEvent event) {
+    }
+
+    @FXML
+    private void modificar(ActionEvent event) {
+    }
+
+    @FXML
+    private void eliminar(ActionEvent event) {
+    }
+
+    @FXML
+    private void guardar(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+    }
+
+}
