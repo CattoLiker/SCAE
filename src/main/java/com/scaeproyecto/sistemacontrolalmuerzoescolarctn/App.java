1f42624a22ed9bcc4990cf3b94a8a3f03400a711
Ian <>
2025-08-14 08:28:52 -0300

    Menu más lindo, solo modificado el font ligeramente

--- src/main/java/com/scaeproyecto/sistemacontrolalmuerzoescolarctn/App.java
+++ src/main/java/com/scaeproyecto/sistemacontrolalmuerzoescolarctn/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("menu"));
+        scene = new Scene(loadFXML("menu")); // yeah
         stage.setScene(scene);
         stage.show();
     }
