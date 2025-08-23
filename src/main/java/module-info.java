46fb0f188b2bfaaf7c38ece3fc451bffc4535621
abiga <abiga@LAPTOP-P07JI1RI>
2025-08-22 23:55:37 -0300

    agregue java.desktop para poder usar javax.swing para las alertas

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.scaeproyecto.sistemacontrolalmuerzoescolarctn {
     requires javafx.fxml;
     requires java.sql;
     requires java.base;
-    
+    requires java.desktop;
     opens com.scaeproyecto.sistemacontrolalmuerzoescolarctn to javafx.fxml;
     exports com.scaeproyecto.sistemacontrolalmuerzoescolarctn;
 }
